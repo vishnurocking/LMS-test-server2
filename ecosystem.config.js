1ac6d48fe2ed486b31ed0410593967bd1287c108
Vishnu S <vishnuitsrocking@gmail.com>
2024-08-18 16:43:39 +0530

    Add simple env &  CICD

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,21 +1,11 @@
 module.exports = {
   apps: [
     {
-      name: "lms-server-dev",
-      script: "npm",
-      args: "run dev",
-      watch: true,
-      env: {
-        NODE_ENV: "development",
-        PORT: 8000,
-      },
-    },
-    {
-      name: "lms-server-prod",
+      name: "lms-server",
       script: "npm",
       args: "start",
+      watch: true,
       env: {
-        NODE_ENV: "production",
         PORT: 8000,
       },
     },
